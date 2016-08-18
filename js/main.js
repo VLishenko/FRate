5154f5caba156ecfa29bc2afa95fb02b9c801c37
Lishenko Vitaliy <lishenkovitaliy@gmail.com>
2016-08-18 17:58:30 +0300

    Finish user_profile_sex

--- js/main.js
+++ js/main.js
@@ -1,71 +1,10 @@
-
-// Extend the prototype
-// (function() {
-
-// var proto = $.circleProgress.defaults,
-//     superInitWidget = proto.initWidget,
-//     superDrawFrame = proto.drawFrame;
-
-// $.extend(proto, {
-//   dotRadius: 10,
-
-//   initWidget: function() {
-//     superInitWidget.call(this);
-//     this.dotOffset = this.dotRadius - this.getThickness() / 2;
-//     this.radius -= this.dotOffset;
-//   },
-
-//   drawFrame: function(v) {
-//     this.ctx.save();
-//     this.ctx.clearRect(0, 0, this.size, this.size);
-//     this.ctx.translate(this.dotOffset, this.dotOffset);
-//     superDrawFrame.call(this, v);
-//     this.drawDot(v);
-//     this.ctx.restore();
-//   },
-
-//   drawDot: function(v) {
-//     var ctx = this.ctx,
-//         sa = this.startAngle + 2 * Math.PI * (v || 0),
-//         r = this.radius,
-//         rd = r - this.getThickness() / 2,
-//         x = r + rd * Math.cos(sa),
-//         y = r + rd * Math.sin(sa);
-
-//     ctx.save();
-//     ctx.fillStyle = this.arcFill;
-//     ctx.beginPath();
-//     ctx.arc(x, y, this.dotRadius, 0, 2 * Math.PI);
-//     ctx.fill();
-//     ctx.restore();
-//   }
-// });
-// var circle = $('.circle'),
-//     value = circle.children('.value');
-
-// circle.circleProgress().on('circle-animation-progress', function (e, p, v) {
-//   value.text((v * 100).toFixed());
-// });
-
-// })();
-
-// $('.circle').circleProgress({
-//   dotRadius: 8,
-//   size: 232,
-//   thickness: 1,
-//   startAngle: Math.PI / 2,
-//   fill: {
-//     color: "#303946"
-//   }
-// });
-
 $(function(){
     $('.truncate').succinct({
         size: 205
     });
 });
 
-// CIRCLE BAR ANIMATION
+// CIRCLE BAR ANIMATION user_profile
 const bar = new ProgressBar({
   percent: 84
 });
@@ -97,3 +36,38 @@ bar_fourth.go();
 $("#chart-animation-five").append(bar_fifth.makeChart() );
 bar_fifth.go();
 
+// CIRCLE BAR ANIMATION user_profile_sex
+const bar_sex_first = new ProgressBar({
+  percent: 84
+});
+
+const bar_sex_second = new ProgressBar({
+  percent: 52
+});
+
+const bar_sex_thierd = new ProgressBar({
+  percent: 50
+});
+
+const bar_sex_fourth = new ProgressBar({
+  percent: 26
+});
+
+const bar_sex_fifth= new ProgressBar({
+  percent: 75
+});
+
+$("#chart-animation-sex-one").append(bar_sex_first.makeChart() );
+bar_sex_first.go();
+
+$("#chart-animation-sex-two").append(bar_sex_second.makeChart() );
+bar_sex_second.go();
+
+$("#chart-animation-sex-three").append(bar_sex_thierd.makeChart() );
+bar_sex_thierd.go();
+
+$("#chart-animation-sex-four").append(bar_sex_fourth.makeChart() );
+bar_sex_fourth.go();
+
+$("#chart-animation-sex-five").append(bar_sex_fifth.makeChart() );
+bar_sex_fifth.go();
