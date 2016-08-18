ea27d912b48d538e380eae4e17c2612bc44e2099
Lishenko Vitaliy <lishenkovitaliy@gmail.com>
2016-08-18 14:11:53 +0300

    Finish user_profile.html

--- js/main.js
+++ js/main.js
@@ -66,103 +66,34 @@ $(function(){
 });
 
 // CIRCLE BAR ANIMATION
-"use strict";
+const bar = new ProgressBar({
+  percent: 84
+});
+const bar_second = new ProgressBar({
+  percent: 51
+});
+const bar_thierd = new ProgressBar({
+  percent: 75
+});
+const bar_fourth = new ProgressBar({
+  percent: 26
+});
+const bar_fifth = new ProgressBar({
+  percent: 52
+});
 
-const settings = {
-    percent: 78
-  , duration: 700
-  , angle: 0
-};
+$("#chart-animation-one").append( bar.makeChart() );
+bar.go();
 
+$("#chart-animation-two").append( bar_second.makeChart() );
+bar_second.go();
 
-// From here: https://gist.github.com/gre/1650294
-const easing = {
-  easeInOutQuad: t => t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
-  easeInOutCubic: t => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
-};
+$("#chart-animation-three").append( bar_thierd.makeChart() );
+bar_thierd.go();
 
+$("#chart-animation-four").append( bar_fourth.makeChart() );
+bar_fourth.go();
 
-const svg = document.querySelector(".stat")
-  , path = svg.querySelector(".stat__path_back")
-  , bar = svg.querySelector(".stat__path_bar")
-  , dot = svg.querySelector(".stat__dot")
-  , text = svg.querySelector(".stat__text")
-  , circleLength = 2 * Math.PI * path.getAttribute("r")
-  , factor = (360 - settings.angle * 2) / 360
-  , getPercentLength = (len, percent, factor = 1) => len / 100 * percent * factor
-;
+$("#chart-animation-five").append(bar_fifth.makeChart() );
+bar_fifth.go();
 
-let animationProgress
-  , animationStart
-;
-
-
-// Set an initial circles settings
-path.style.strokeDasharray = bar.style.strokeDasharray = circleLength;
-path.style.strokeDashoffset = bar.style.strokeDashoffset = circleLength;
-
-
-/**
- * Animation callback function for drawing a circle path
- *
- * @param {Element}   SVG circle element
- * @param {Number}    Completion rate [0..1]
- * @param {Number}    Filling percentage
- * @param {Function}  Easing function
- */
-const draw = (e, t, p, fn) => e.style.strokeDashoffset = circleLength - getPercentLength(circleLength, p * fn(t), factor);
-
-/**
- * Animation callback function for rotating a circle path
- *
- * @param {Element}   SVG circle element
- * @param {Number}    Completion rate [0..1]
- * @param {Number}    Angle of the rotation
- * @param {Number}    x-position of the center of rotation
- * @param {Number}    y-position of the center of rotation
- * @param {Function}  Easing function
- */
-const rotate = (e, t, a, x, y, fn) => e.setAttribute("transform", `rotate(${fn(t) * a} ${x} ${y})`);
-
-
-const go = () => {
-
-  // Oops! CSS transforms on SVG elements doesn't works in IE
-  /*
-    path.style.transitionDuration = bar.style.transitionDuration = `${settings.duration}ms`;
-    requestAnimationFrame(() => path.style.transform = bar.style.transform = `rotate(${settings.angle}deg)`);
-  */
-
-  const animate = () => {
-    animationProgress = (Date.now() - animationStart) / settings.duration;
-
-    if (animationProgress >= 1) {
-      animationProgress = 1;
-    }
-
-    // dot's rotation is here
-    const dotRotateAngle = 360 * easing.easeInOutCubic(animationProgress) * settings.percent / 100;
-    dot.setAttribute("transform", `rotate(${dotRotateAngle} 100 100)`);
-
-    // ok, let's draw
-    draw(path, animationProgress, 100, easing.easeInOutQuad);
-    draw(bar, animationProgress, settings.percent, easing.easeInOutCubic);
-
-    if (settings.angle) {
-      rotate(path, animationProgress, settings.angle, 100, 100, easing.easeInOutQuad);
-      rotate(bar, animationProgress, settings.angle, 100, 100, easing.easeInOutQuad);
-    }
-
-    text.textContent = Math.floor(settings.percent * easing.easeInOutQuad(animationProgress)) + '%';
-
-
-    if (animationProgress !== 1) {
-      requestAnimationFrame(animate);
-    }
-  };
-
-  animationStart = Date.now();
-  animate();
-};
-
-go();
